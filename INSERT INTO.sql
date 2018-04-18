e761f79f97c36b9f49e93767ae445b5a9eea5a99
hrolfurgylfa <hrolfurgylfa@gmail.com>
2018-04-18 09:39:00 +0000

    INSERT INTO skipanir með 3 röðum komnar

---
+++ INSERT INTO.sql
@@ -0,0 +1,21 @@
+INSERT INTO Lokaverkefni.songvarar
+	(nafn,aldur,þjoderni)
+VALUES
+	("Hatsune Miku",16,""),
+    ("Hatsune Miku English",16,""),
+    ("Hatsune Miku English V3",16,"");
+    
+INSERT INTO Lokaverkefni.hofundar
+	(nafn,aldur,þjoderni)
+VALUES
+	("GuitarHeroPianoZero",31,"Japan"),
+    ("Circus-P",26,"Canada"),
+    ("sasakure.UK",23,"UK");
+
+
+INSERT INTO Lokaverkefni.log
+	(heiti,songvari,hofundur,lengd)
+VALUES
+	("Glass Wall","Hatsune Miku English","GuitarHeroPianoZero",281),
+    ("Goodbye","Hatsune Miku English V3","Circus-P",205),
+    ("Hello, Planet","Hatsune Miku","sasakure.UK",308);

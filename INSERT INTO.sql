6057d6aebec9d5d57677bc78f20cb7eb320b860e
hrolfurgylfa <hrolfurgylfa@gmail.com>
2018-04-27 14:11:59 +0000

    Komid 3 reitir um hatsune miku í flytjandi, hofundur og utgefandi

--- INSERT INTO.sql
+++ INSERT INTO.sql
@@ -1,19 +1,24 @@
-INSERT INTO Lokaverkefni.songvarar
-	(nafn,aldur,þjoderni)
+INSERT INTO Lokaverkefni.flytjandi
+	(nafn,faedingardagur,faedingarmanudur,faedingarar,aldur,danardagur,lysing,þjoderni)
 VALUES
-	("Hatsune Miku",16,""),
-    ("Hatsune Miku English",16,""),
-    ("Hatsune Miku English V3",16,"");
+	("Hatsune Miku",31,8,2007,16,,"Hatsune Miku (Japanese: 初音ミク), is the name of a Vocaloid software voicebank developed by Crypton Future Media, and its official moe anthropomorphism, a 16-year-old girl with long, turquoise twintails. She uses Yamaha Corporation's Vocaloid 2, Vocaloid 3 and Vocaloid 4 singing synthesizing technologies. She also uses Crypton Future Media's Piapro Studio, a singing synthesizer VSTi Plugin. She was the second Vocaloid sold using the Vocaloid 2 engine, and the first Japanese Vocaloid to use the Japanese version of the Vocaloid 2 engine. Her voice is modeled from Japanese voice actress Saki Fujita. Hatsune Miku's personification has been marketed as a virtual idol, and has performed at concerts onstage as an animated projection (rear cast projection on a specially coated glass screen).","Japan");
     
-INSERT INTO Lokaverkefni.hofundar
-	(nafn,aldur,þjoderni)
+INSERT INTO Lokaverkefni.hofundur
+	(nafn,faedingardagur,faedingarmanudur,faedingarar,aldur,danardagur,þjoderni)
 VALUES
-	("GuitarHeroPianoZero",31,"Japan"),
-    ("Circus-P",26,"Canada"),
-    ("sasakure.UK",23,"UK");
+	("GuitarHeroPianoZero",,,,31,"Japan"),
+    ("Circus-P",,,,26,"Canada"),
+    ("sasakure.UK",,,,23,"UK");
+    
+INSERT INTO Lokaverkefni.utgefandi
+	(nafn,stefna,copyright,Staðsetning)
+VALUES
+	("GuitarHeroPianoZero","FILLA ÚT","Nei","Japan"),
+    ("Circus-P","FILLA ÚT","Nei","Canada"),
+    ("sasakure.UK","FILLA ÚT","Nei","UK");
 
 
-INSERT INTO Lokaverkefni.log
+INSERT INTO Lokaverkefni.lagalisti
 	(heiti,songvari,hofundur,lengd)
 VALUES
 	("Glass Wall","Hatsune Miku English","GuitarHeroPianoZero",281),

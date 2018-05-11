76b7c3678d644737b5603acf3520ed648a88e3bc
Frillion <fridriksoebech@gmail.com>
2018-05-11 22:07:22 +0000

    gerði spurningu 1,3,5

--- Skipanir.sql
+++ Skipanir.sql
@@ -1,2 +1,13 @@
-SELECT nafn, aldur, nafn_lags FROM Lokaverkefni.lagalisti
-JOIN flytjandi ON flytjandi.nafn = lagalisti.songvari;
+SELECT nafn_lags,utgefandi FROM Lokaverkefni.lagalisti
+JOIN utgefandi ON utgefandi.nafn = lagalisti.utgefandi;
+
+SELECT nafn_lags,lengd FROM Lokaverkefni.lagalisti
+WHERE
+tegund = "rapp";
+
+SELECT lag FROM Lokaverkefni.lagaval
+WHERE
+lag = MAX(lag);
+
+/*SELECT nafn, aldur, nafn_lags FROM Lokaverkefni.lagalisti
+JOIN flytjandi ON flytjandi.nafn = lagalisti.songvari;*/

731ef71e0ab5ab9f3ede72bb9d7dbd285f51f1fc
hrolfurgylfa <hrolfurgylfa@gmail.com>
2018-05-13 14:00:47 +0000

    Spurningar 2, 4, 6, 8 og 10 komnar

--- Skipanir.sql
+++ Skipanir.sql
@@ -1,33 +1,60 @@
-SELECT nafn_lags,utgefandi FROM Lokaverkefni.lagalisti#Eftirspurn 1
-JOIN utgefandi ON utgefandi.nafn = lagalisti.utgefandi;
+#Eftirspurn 1
+#SELECT nafn_lags,utgefandi FROM Lokaverkefni.lagalisti
+#JOIN utgefandi ON utgefandi.nafn = lagalisti.utgefandi;
 
-SELECT nafn_lags,lengd FROM Lokaverkefni.lagalisti#Eftirspurn 3
-WHERE
-tegund = "rapp";
+#Eftirspurn 2
+#SELECT lagaval_ID,Tonleikanumer_ID FROM Lokaverkefni.lagaval WHERE lag = "Goodbye";
 
-SELECT lag FROM Lokaverkefni.lagaval#Eftirspurn 5
-GROUP BY lag
-ORDER BY COUNT(lag) DESC
-LIMIT 1;
+#Eftirspurn 3
+#SELECT nafn_lags,lengd FROM Lokaverkefni.lagalisti
+#WHERE tegund = "rapp";
 
-SELECT nafn_lags,songvari FROM	Lokaverkefni.lagalisti#Eftirspurn 7
-JOIN flytjandi ON flytjandi.nafn = lagalisti.songvari
-GROUP BY lengd
-LIMIT 5;
+#Eftirspurn 4
+#SELECT nafn_lags,lengd,utgafuar,utgafudagur,utgafuar FROM Lokaverkefni.lagalisti WHERE lengd > 300 ORDER BY lengd;
 
-SELECT askrifandi,COUNT(lag) AS Lög_askrifanda FROM Lokaverkefni.lagaval#Eftirspurn 9
-JOIN askrifandi ON askrifandi.nafn = lagalisti.askrifandi
-GROUP BY askrifandi;
+#Eftirspurn 5
+#SELECT lag FROM Lokaverkefni.lagaval
+#GROUP BY lag
+#ORDER BY COUNT(lag) DESC
+#LIMIT 1;
 
-SELECT nafn_lags,utgefandi FROM Lokaverkefni.lagalisti#Eftirspurn 11
-JOIN utgefandi ON utgefandi.nafn = lagalisti.utgefandi
-WHERE
-utgafuar < 2015;
+#Eftirspurn 6
+#SELECT askrifandi,lagaval_ID,dagsetning FROM Lokaverkefni.lagaval;
 
-SELECT nafn, lysing FROM Lokaverkefni.flytjandi#Eftirspurn 13
-WHERE
-COUNT(lagalisti.nafn_lags) > 4
-GROUP BY (lagalisti.songvari);
+#Eftirspurn 7
+#SELECT nafn_lags,songvari FROM	Lokaverkefni.lagalisti
+#JOIN flytjandi ON flytjandi.nafn = lagalisti.songvari
+#GROUP BY lengd
+#LIMIT 5;
+
+#Eftirspurn 8
+#SELECT tegund,Count(tegund) AS "Fjöldi í tegund" FROM Lokaverkefni.lagalisti GROUP BY tegund ORDER BY Count(tegund) DESC LIMIT 3;
+
+#Eftirspurn 9
+#SELECT askrifandi,COUNT(lag) AS Lög_askrifanda FROM Lokaverkefni.lagaval
+#JOIN askrifandi ON askrifandi.nafn = lagalisti.askrifandi
+#GROUP BY askrifandi;
+
+#Eftirspurn 10
+#SELECT nafn_lags FROM Lokaverkefni.lagalisti WHERE nafn_lags LIKE "A%s%"
+
+#Eftirspurn 11
+#SELECT nafn_lags,utgefandi FROM Lokaverkefni.lagalisti
+#JOIN utgefandi ON utgefandi.nafn = lagalisti.utgefandi
+#WHERE
+#utgafuar < 2015;
+
+#Eftirspurn 12
+SELECT FROM Lokaverkefni.
+
+#Eftirspurn 13
+#SELECT nafn, lysing FROM Lokaverkefni.flytjandi
+#WHERE
+#COUNT(lagalisti.nafn_lags) > 4
+#GROUP BY (lagalisti.songvari);
+
+#Eftirspurn 14
+#SELECT FROM Lokaverkefni.
 
 /*SELECT nafn, aldur, nafn_lags FROM Lokaverkefni.lagalisti
 JOIN flytjandi ON flytjandi.nafn = lagalisti.songvari;*/
